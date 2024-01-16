206da21ff0d1a8ba87e7e330ff752a906779bfd2
Eunice <jes.e.0927@gmail.com>
2024-01-16 15:18:54 +0900

    Feat: 새로운 letter 데이터를 추가할 수 있다
    - LetterListView의 Button을 누르면 미리 설정된 데이터가 현재 로그인해 있는 유저의 문서 아래의 collection인 letters에 새 문서로 저장된다.

--- LetterProjectFirebaseExample/Core/Data/View/LetterListView.swift
+++ LetterProjectFirebaseExample/Core/Data/View/LetterListView.swift
@@ -8,8 +8,16 @@
 import SwiftUI
 
 struct LetterListView: View {
+    @EnvironmentObject var viewModel: AuthViewModel
+    @EnvironmentObject var letterViewModel: FirestoreViewModel
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+
+        Button {
+            letterViewModel.addLetter(writer: "me", recipient: "you", summary: "helooo", date: Date())
+        } label: {
+            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        }
     }
 }
 
